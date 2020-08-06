2034b63d226ccfe3ddcbb5b53b5db8627c2d50b3
Akshay <43185089+akshay2742@users.noreply.github.com>
2020-08-06 23:04:43 +0530

    Add files via upload

--- contact.php
+++ contact.php
@@ -15,9 +15,9 @@ if(empty($name)||empty($visitor_email))
 
 $email_from = 'akshays274@gmail.com';
 $email_subject = "New Form Submission";
-$email_body = "You have received a new message from $name.\n"."email address: $visitor_email\n"."Here is the message: \n $message";
+$email_body = "You have received a new message from ".$name."\n"."email address: ".$visitor_email."\n"."Here is the message: ".$message;
     
 $to = "akshays274@gmail.com";
-$headers = "From: $email_from". "\r\n"."Reply-To: akshays274@gmail.com"."\r\n"."X-Mailer: PHP/".phpversion();
+$headers = "From: ".$email_from;
 
-mail($to,$email_subject,$email_body,$headers);
+mail($to, $email_subject, $email_body, $headers);
