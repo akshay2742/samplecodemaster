2fb5d9463837e7f358757aa2fecd82558bdb42e6
Akshay <43185089+akshay2742@users.noreply.github.com>
2020-08-06 23:36:52 +0530

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,24 +1,24 @@
 <?php
-if(!isset($_POST['submit']))
+if(isset($_POST['submit']))
 {
-    echo "error: you need to submit the form!";
+    $name=$_POST['name'];
+    $visitor_email=$_POST['email'];
+    $message=$_POST['message'];
+
+    $email_from = 'akshays274@gmail.com';
+    $email_subject = 'New Form Submission';
+    $email_body = "You have received a new message from ".$name."\n"."email address: ".$visitor_email."\n"."Here is the message: ".$message;
+
+    $to = 'akshays274@gmail.com';
+    $headers = "From: ".$email_from;
+
+    if(mail($to, $email_subject, $email_body, $headers))
+    {
+        echo "<h1>Sent Successfully</h1>";
+    }
+    else
+    {
+        echo "Something went wrong";
+    }
 }
-$name=$_POST['name'];
-$visitor_email=$_POST['email'];
-$message=$_POST['message'];
-
-if(empty($name)||empty($visitor_email))
-{
-    echo "Name and email are mandatory!";
-    exit;
-}
-
-$email_from = 'akshays274@gmail.com';
-$email_subject = 'New Form Submission';
-$email_body = "You have received a new message from ".$name."\n"."email address: ".$visitor_email."\n"."Here is the message: ".$message;
-    
-$to = 'akshays274@gmail.com';
-$headers = "From: ".$email_from;
-
-mail($to, $email_subject, $email_body, $headers);
 ?>
