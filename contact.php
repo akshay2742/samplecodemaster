f76730efec1f5dc0daa0e83a443f1e072eaf0cdb
Akshay <43185089+akshay2742@users.noreply.github.com>
2020-08-06 20:52:21 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php
+if(!isset($_POST['submit']))
+{
+    echo "error: you need to submit the form!";
+}
+$name=$_POST['name'];
+$visitor_email=$_POST['email'];
+$message=$_POST['message'];
+
+if(empty($name)||empty($visitor_email))
+{
+    echo "Name and email are mandatory!";
+    exit;
+}
+
+$email_from = 'akshays274@gmail.com';
+$email_subject = "New Form Submission";
+$email_body = "You have received a new message from $name.\n"."email address: $visitor_email\n"."Here is the message: \n $message";
+    
+$to = "akshays274@gmail.com";
+$headers = "From: $email_from". "\r\n"."Reply-To: akshays274@gmail.com"."\r\n"."X-Mailer: PHP/".phpversion();
+
+mail($to,$email_subject,$email_body,$headers);
